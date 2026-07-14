78fb88ee60fa306e503e12b40217be5707905dbd
edwanadev <adoralenou@gmail.com>
2026-07-14 09:33:43 +0100

    fix

--- Niveau_intermédière/index_in.js
+++ Niveau_intermédière/index_in.js
@@ -1,11 +1,31 @@
+const btn = document.getElementById("btn");
+const aler = document.getElementById("alert");
+const change = document.getElementById("change");
+const visible = document.getElementById("title");
+const add = document.getElementById("btn_f");
 
-const btn=document.getElementById("btn")
-const text=document.querySelector(".text")
-const aler=document.getElementById("alert")
-btn.addEventListener("click",()=>{
-    let elt=document.createElement("div")
-    elt.innerHTML=`<p class="text-3xl"> élement </p`
-})
-aler.addEventListener("click",()=>{
-    alert("hi!")
-})
+btn.addEventListener("click", () => {
+  const text = document.querySelector(".text");
+  const textContent = text.value.trim();
+  if (textContent !== "") {
+    let elt = document.createElement("div");
+    elt.innerHTML += `<p class="elt"> ${textContent} </p>`;
+    const remove = elt.querySelector(".elt");
+    remove.addEventListener("click", () => {
+      elt.remove();
+    });
+    document.querySelector(".container").appendChild(elt);
+    text.value = "";
+  }
+});
+aler.addEventListener("click", () => {
+  alert("hi!");
+});
+
+change.addEventListener("keypress", () => {
+  change.style.background = "purple";
+});
+add.addEventListener("click", () => {
+  visible.style.visibility = "visible";
+  add.classList.add("active");
+});
